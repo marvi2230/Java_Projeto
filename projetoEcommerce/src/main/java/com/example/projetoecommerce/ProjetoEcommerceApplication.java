d6051c75ab89e2f0e42bae600fb0381b55126d2f
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2024-05-28 21:06:10 -0300

    Primeiro commit

---
+++ projetoEcommerce/src/main/java/com/example/projetoecommerce/ProjetoEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEcommerceApplication.class, args);
+	}
+
+}
